959e4df044676776068dda760f9b997c9db3354c
rkynn <rakeyannuramria@gmail.com>
2020-09-29 14:51:37 +0700

    + Materi Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/black.jpg" alt="" width="150" height="150" class="rounded-circle shadow">
     <p>Halo Nama saya <?= $data['nama'];?>, Umur saya <?= $data['umur'];?> Tahun, saya adalah <?= $data['pekerjaan'];?></p>
+</div>
